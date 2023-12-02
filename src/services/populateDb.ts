5aeae9456cccc6a4e60c565bd60dcf7fbee15417
Guilherme Leal <guiherme.leal.5342@gmail.com>
2023-12-02 01:33:57 -0300

    parser pronto

--- src/services/populateDb.ts
+++ src/services/populateDb.ts
@@ -1,11 +1,8 @@
-import csv from "csv";
 import fs from "fs";
-import dbClient from "./db";
 
 export const populateDb = async () => {
-	const parser = csv.parse({ delimiter: ";" });
-
 	const tables = [
+		"socios",
 		"empresas",
 		"estabelecimentos",
 		// "estados",
@@ -14,13 +11,12 @@ export const populateDb = async () => {
 		// "naturezas",
 		// "paises",
 		// "qualificacoes",
-		// "simples",
-		"socios",
+		"simples",
 	];
 
 	for (const table of tables) {
 		for (const file of fs.readdirSync("./csv/" + table)) {
-			fs.readFile(`./csv/${table}/${file}`, "utf8", async (err, data) => {
+			fs.readFile(`./csv/${table}/${file}`, "utf8", async (_, data) => {
 				// create sql file if not exists
 				if (!fs.existsSync("./sql")) {
 					fs.mkdirSync("./sql");
@@ -41,7 +37,25 @@ export const populateDb = async () => {
 
 					if (data === "") return;
 
-					const sql = `INSERT INTO ${table} VALUES (${data});\n`;
+					let sql = "";
+					switch (table) {
+						case "empresas": {
+							sql = `INSERT INTO ${table} (cnpj_basico, razao_social, natureza_juridica, qualificacao_responsavel, capital_social, porte, ente_federativo_responsavel) VALUES (${data});\n`;
+							break;
+						}
+						case "estabelecimentos": {
+							sql = `INSERT INTO ${table} (cnpj_basico, cnpj_ordem, cnpj_dv, identificador_matriz_filial, situacao_cadastral, data_situacao_cadastral, motivo_situacao_cadastral, nome_cidade_exterior, pais, data_inicio_atividade, cnae_principal, cnae_secundaria, tipo_logradouro, logradouro, numero, complemento, bairro, cep, uf, municipio, ddd_1, telefone_1, ddd_2, telefone_2, ddd_fax, fax, email, situacao_especial, data_situacao_especial) VALUES (${data});\n`;
+							break;
+						}
+						case "simples": {
+							sql = `INSERT INTO ${table} (cnpj_basico, opcao_simples, data_opcao, data_exclusao, opcao_mei, data_opcao_mei, data_exclusao_mei) VALUES (${data});\n`;
+							break;
+						}
+						case "socios": {
+							sql = `INSERT INTO ${table} (cnpj_basico, identificador_socio, nome, cpf_cnpj, qualificacao, data_entrada, pais, cpf_representante_legal, nome_representante_legal, qualificacao_representante_legal, faixa_etaria) VALUES (${data});\n`;
+							break;
+						}
+					}
 					fs.appendFileSync(`./sql/${table}.sql`, sql);
 				});
 			});
