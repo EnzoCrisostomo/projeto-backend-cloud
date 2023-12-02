ef9b4f28d47f1bbafaed235eb2fcadc0484db80c
Guilherme Leal <guiherme.leal.5342@gmail.com>
2023-12-02 01:18:21 -0300

    parser

--- src/services/populateDb.ts
+++ src/services/populateDb.ts
@@ -5,46 +5,17 @@ import dbClient from "./db";
 export const populateDb = async () => {
 	const parser = csv.parse({ delimiter: ";" });
 
-	//read entire csv folder
-	const csvs = fs.readdirSync("./csv");
-
-	// for (const table of csvs) {
-	//read each file in folder
-	// const folder = fs.readdirSync(`./csv/${table}`);
-
-	// for (const file of folder) {
-	// 	fs.createReadStream(`./csv/${table}/${file}`)
-	// 		.pipe(parser)
-	// 		.on("data", async (row) => {
-	// 			// //insert each line in database
-	// 			// await dbClient.query(`INSERT INTO ${table} VALUES (${row.map((r) => `'${r}'`).join(",")})`);
-
-	// 			//create sql file if not exists
-	// 			if (!fs.existsSync("./sql")) {
-	// 				fs.mkdirSync("./sql");
-	// 			}
-	// 			if (!fs.existsSync(`./sql/${table}.sql`)) {
-	// 				fs.writeFileSync(`./sql/${table}.sql`, "");
-	// 			}
-
-	// 			// Save onto sql file
-	// 			const sql = `INSERT INTO ${table} VALUES (${row.map((r: any) => `'${r}'`).join(",")});\n`;
-	// 			fs.appendFileSync(`./sql/${table}.sql`, sql);
-
-	// 		});
-	// }
-
 	const tables = [
-		// "empresas",
-		// "estabelecimentos",
-		"estados",
-		"motivos",
-		"municipios",
-		"naturezas",
-		"paises",
-		"qualificacoes",
-		"simples",
-		// "socios",
+		"empresas",
+		"estabelecimentos",
+		// "estados",
+		// "motivos",
+		// "municipios",
+		// "naturezas",
+		// "paises",
+		// "qualificacoes",
+		// "simples",
+		"socios",
 	];
 
 	for (const table of tables) {
@@ -57,17 +28,20 @@ export const populateDb = async () => {
 				if (!fs.existsSync(`./sql/${table}.sql`)) {
 					fs.writeFileSync(`./sql/${table}.sql`, "");
 				}
+
+				if (!data) return;
+
 				// Save onto sql file
-				const rows = data.split("\n");
-				const columns = rows[0].split(";");
-
-				rows.shift();
-
-				rows.forEach((row) => {
-					const sql = `INSERT INTO ${table} (${columns.join(",")}) VALUES (${row
+				data.split("\n").forEach((row) => {
+					const data = row
 						.split(";")
-						.map((r: any) => `'${r}'`)
-						.join(",")});\n`;
+						.map((r) => `${r.replaceAll("'", "''").replaceAll('"', "'")}`)
+						.join(",")
+						.replace(/(\r\n|\n|\r)/gm, "");
+
+					if (data === "") return;
+
+					const sql = `INSERT INTO ${table} VALUES (${data});\n`;
 					fs.appendFileSync(`./sql/${table}.sql`, sql);
 				});
 			});
